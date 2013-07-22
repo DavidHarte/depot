3b8c8a8e90594356c749bb839f0261bba386727a
David Harte <david_j_harte@hotmail.com>
2013-07-22 14:51:08 +0100

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '106e55c2c64dbe106f4f02e7b3b40f9b28fcb7e65dd716dc73a2d3b5fcf555221543d8d50c9bc90fb516271facdcb76502b9e46138361c56bcb1eff0f31db8cf'
---
+++ db/migrate/20130715132835_remove.rb
@@ -0,0 +1,8 @@
+class Remove < ActiveRecord::Migration
+  def up
+  end
+remove_column :orders, :/
+  def down
+  	
+  end
+end
